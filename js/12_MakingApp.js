fdc670c67cc2ed31a31b3637230d18fb74b2cb20
Pavel <bordertouch@gmail.com>
2021-04-19 11:39:27 +0300

    app mod

--- js/12_MakingApp.js
+++ js/12_MakingApp.js
@@ -1,4 +1,4 @@
-const numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
 console.log(numberOfFilms);
 
 const personalMovieDB = {
@@ -10,13 +10,33 @@ const personalMovieDB = {
 };
 
 
-const a = prompt('Какой последний фильмы вы посмотрели?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Какой последний фильмы вы посмотрели?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Какой последний фильмы вы посмотрели?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Какой последний фильмы вы посмотрели?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
 
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено немного фильмов');
+} else if (10 <= personalMovieDB.count && personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    alert('Вы киноман');
+} else {
+    alert('Ошибочка');
+}
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Какой последний фильмы вы посмотрели?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+          i--;
+    }
+}
